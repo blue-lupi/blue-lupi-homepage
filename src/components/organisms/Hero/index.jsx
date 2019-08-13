ff188f06a073461b6c77d9a7a2c37b53f325b81d
Christian Aichner <contact@aichner-christian.com>
2019-08-14 01:59:03 +0200

    Fix center line
    The center line is now centered.

--- src/components/organisms/Hero/index.jsx
+++ src/components/organisms/Hero/index.jsx
@@ -23,22 +23,31 @@ import bg1 from '../../../assets/content/bg/bg1.jpg';
 class Hero extends React.Component{
     render(){
         return(
-            <MDBRow className="w-100 m-0">
-                <MDBCol md="6" className="pl-0">
-                    <MDBView src={bg1} >
-                        <MDBMask pattern={5} className="rgba-purple-slight d-flex justify-content-center align-items-center"></MDBMask>
+            <div id="hero">
+                <div className="overlay">
+                    <MDBView src={lupiLogoWhite} >
+                        <MDBMask className="d-flex justify-content-center align-items-center"></MDBMask>
                     </MDBView>
-                </MDBCol>
-                <MDBCol md="6" className="hero-side pr-0">
-                    <div>
-                        <h2>More</h2>
-                        <h2>than</h2>
-                        <h2>Coffee</h2>
-                        <span>It's an intellectual drink,</span><span>for the chosen ones.</span>
-                        <MDBBtn color="lupi-blue" rounded>Take a sip<MDBIcon icon="arrow-right" className="pl-2"/></MDBBtn>
-                    </div>
-                </MDBCol>
-            </MDBRow>
+                </div>
+                <MDBRow className="w-100 m-0">
+                    <MDBCol md="6" className="pl-0">
+                        <MDBView src={bg1} >
+                            <MDBMask pattern={5} className="rgba-purple-slight d-flex justify-content-center align-items-center"></MDBMask>
+                        </MDBView>
+                    </MDBCol>
+                    <MDBCol md="6" className="pr-0">
+                        <div className="hero-side">
+                            <div>
+                                <h2>More</h2>
+                                <h2>than</h2>
+                                <h2>Coffee</h2>
+                                <span>It's an intellectual drink,</span><span>for the chosen ones.</span>
+                                <MDBBtn color="lupi-blue" rounded>Take a sip<MDBIcon icon="arrow-right" className="pl-2"/></MDBBtn>
+                            </div>
+                        </div>
+                    </MDBCol>
+                </MDBRow>
+            </div>
         );
     }
 }
