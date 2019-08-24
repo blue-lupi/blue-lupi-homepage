ebdeb4bc7efcb99a36d475005b2e65cb9b77d091
Aichnerc <contact@aichner-christian.com>
2019-08-24 14:09:30 +0200

    Add Section Trust structure
    The basic structure of the trust section has
    been added.

--- src/components/organisms/Sections/Trust/index.jsx
+++ src/components/organisms/Sections/Trust/index.jsx
@@ -9,23 +9,35 @@ import { Parallax } from 'react-scroll-parallax';
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
 import {
-    MDBView,
-    MDBMask,
     MDBRow,
     MDBCol,
-    MDBBtn,
-    MDBIcon,
-    MDBAnimation,
 } from 'mdbreact';
 
 //> Images
 // To be added
 
+const companies = [
+    { name: "Test 1", url: "www.test1.at", image: "test1.jpg" },
+    { name: "Test 2", url: "www.test2.at", image: "test2.jpg" },
+    { name: "Test 3", url: "www.test3.at", image: "test3.jpg" },
+    { name: "Test 4", url: "www.test4.at", image: "test4.jpg" },
+]
+
 class Trust extends React.Component{
     render(){
         return(
             <section id="trust">
-
+                <MDBRow className="flex-center m-0">
+                    {companies.map((company, i) => {
+                        return(
+                            <MDBCol md="2" className="text-center">
+                                {company.name}
+                                <p>Logo to be added.</p>
+                            </MDBCol>
+                        )
+                    })}
+                    
+                </MDBRow>
             </section>
         );
     }
