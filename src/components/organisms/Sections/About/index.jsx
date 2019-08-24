3e661a4b9cb2bfb7f543ac4310b0f73879357210
Aichnerc <contact@aichner-christian.com>
2019-08-24 14:53:37 +0200

    Add Section About structure
    The basic structure of the About section has been
    added.

--- src/components/organisms/Sections/About/index.jsx
+++ src/components/organisms/Sections/About/index.jsx
@@ -25,7 +25,51 @@ class About extends React.Component{
     render(){
         return(
             <section id="about">
-
+                <MDBRow className="m-0 p-2">
+                    <MDBCol md="6">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. 
+                        Aliquam erat volutpat. Ut a porta risus, eu porttitor nisi. Pellentesque turpis turpis, 
+                        mattis lobortis justo sagittis, tempus ornare dui.
+                    </MDBCol>
+                    <MDBCol md="6" className="text-right">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. 
+                        Aliquam erat volutpat. Ut a porta risus, eu porttitor nisi. Pellentesque turpis turpis, 
+                        mattis lobortis justo sagittis, tempus ornare dui.
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow className="m-0 p-2">
+                    <MDBCol md="4">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. 
+                        Aliquam erat volutpat. Ut a porta risus, eu porttitor nisi. Pellentesque turpis turpis, 
+                        mattis lobortis justo sagittis, tempus ornare dui.
+                    </MDBCol>
+                    <MDBCol md="4" className="text-center">
+                        Center Icon / Image
+                    </MDBCol>
+                    <MDBCol md="4" className="text-right">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. 
+                        Aliquam erat volutpat. Ut a porta risus, eu porttitor nisi. Pellentesque turpis turpis, 
+                        mattis lobortis justo sagittis, tempus ornare dui.
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow className="m-0 p-2">
+                    <MDBCol md="6">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. 
+                        Aliquam erat volutpat. Ut a porta risus, eu porttitor nisi. Pellentesque turpis turpis, 
+                        mattis lobortis justo sagittis, tempus ornare dui.
+                    </MDBCol>
+                    <MDBCol md="6" className="text-right">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. 
+                        Aliquam erat volutpat. Ut a porta risus, eu porttitor nisi. Pellentesque turpis turpis, 
+                        mattis lobortis justo sagittis, tempus ornare dui.
+                    </MDBCol>
+                </MDBRow>
             </section>
         );
     }
