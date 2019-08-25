9b7af9722e9fa7c6bf8be49f2c26683f993f89b1
Christian Aichner <contact@aichner-christian.com>
2019-08-25 19:20:33 +0200

    Add Instagram helper
    The Instagram Feed / Gallery helper has been
    implemented.
    Ref: https://codesandbox.io/s/pw363kq3nm

---
+++ src/components/helpers/InstagramFeed.jsx
@@ -0,0 +1,69 @@
+//> React
+// Contains all the functionality necessary to define React components
+import React from 'react';
+
+//> MDB
+// "Material Design for Bootstrap" is a great UI design framework
+import {
+    MDBRow,
+    MDBCol,
+} from 'mdbreact';
+
+export default class extends React.Component {
+    state = { photos: [] };
+
+    async componentDidMount() {
+        try {
+        // Hack from https://stackoverflow.com/a/47243409/2217533
+        const response = await fetch(
+            `https://www.instagram.com/graphql/query?query_id=17888483320059182&variables={"id":"${
+            this.props.userId
+            }","first":${this.props.photoCount},"after":null}`
+        );
+        const { data } = await response.json();
+        const photos = data.user.edge_owner_to_timeline_media.edges.map(
+            ({ node }) => {
+            const { id } = node;
+            const caption = node.edge_media_to_caption.edges[0].node.text;
+            const thumbnail = node.thumbnail_resources.find(
+                thumbnail => thumbnail.config_width === this.props.thumbnailWidth
+            );
+            const { src, config_width: width, config_height: height } = thumbnail;
+            const url = `https://www.instagram.com/p/${node.shortcode}`;
+            return {
+                id,
+                caption,
+                src,
+                width,
+                height,
+                url
+            };
+            }
+        );
+        this.setState({ photos });
+        } catch (error) {
+        console.error(error);
+        }
+    }
+
+    render() {
+        return (
+        <MDBRow>
+
+            {this.state.photos &&
+            this.state.photos.map(({ src, url }) => (
+                <MDBCol md="4">
+                    <a href={url} target="_blank">
+                        <img className="img-fluid" src={src} />
+                    </a>
+                </MDBCol>
+            ))}
+        </MDBRow>
+        );
+    }
+}
+
+/** 
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
