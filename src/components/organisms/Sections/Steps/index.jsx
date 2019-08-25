f91e930b5f0ead167f1142dd83c5c839ba19cea8
Christian Aichner <contact@aichner-christian.com>
2019-08-25 20:20:30 +0200

    Update Steps content
    The content of the Steps section has been
    updated.

--- src/components/organisms/Sections/Steps/index.jsx
+++ src/components/organisms/Sections/Steps/index.jsx
@@ -9,6 +9,7 @@ import React from 'react';
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
 import {
+    MDBContainer,
     MDBRow,
     MDBCol,
 } from 'mdbreact';
@@ -20,89 +21,67 @@ class Story extends React.Component{
     render(){
         return(
             <section id="story">
-                <MDBRow className="m-0">
-                    <MDBCol md="6">
-                        !! Image !!
-                    </MDBCol>
-                    <MDBCol md="6" className="pr-5 pl-5">
-                        <h2>Bla bla bla</h2>
-                        <p className="lead">Lead to catch attention</p>
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
-                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. Aliquam 
-                        erat volutpat. Ut a portarisus, eu porttitor nisi. Pellentesque
-                        turpis turpis, mattis lobortis justo sagittis, tempus ornare dui. Sed ornare nulla ac nulla 
-                        dapibus, eget vestibulum velit
-                        faucibus. Etiam auctor, augue ut vehicula porta, dui velit elementum nisi, a viverra enim 
-                        lectus eget nisl. Aenean laoreet
-                        in nibh eu commodo. Etiam augue sapien, tristique eget purus porttitor, egestas interdum 
-                        dolor. Sed odio ante, commodo eget dignissim nec, fermentum sed risus. Quisque imperdiet 
-                        nulla metus, vitae malesuada dolor pellentesque id. Vivamus condimentum consequat mattis. 
-                        Proin volutpat justo at tellus laoreet luctus vel eget risus.
-                        </p>
-                    </MDBCol>
-                    <MDBCol md="6" className="pr-5 pl-5">
-                        <h2>Bla bla bla</h2>
-                        <p className="lead">Lead to catch attention</p>
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
-                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. Aliquam 
-                        erat volutpat. Ut a portarisus, eu porttitor nisi. Pellentesque
-                        turpis turpis, mattis lobortis justo sagittis, tempus ornare dui. Sed ornare nulla ac nulla 
-                        dapibus, eget vestibulum velit
-                        faucibus. Etiam auctor, augue ut vehicula porta, dui velit elementum nisi, a viverra enim 
-                        lectus eget nisl. Aenean laoreet
-                        in nibh eu commodo. Etiam augue sapien, tristique eget purus porttitor, egestas interdum 
-                        dolor. Sed odio ante, commodo eget dignissim nec, fermentum sed risus. Quisque imperdiet 
-                        nulla metus, vitae malesuada dolor pellentesque id. Vivamus condimentum consequat mattis. 
-                        Proin volutpat justo at tellus laoreet luctus vel eget risus.
-                        </p>
-                    </MDBCol>
-                    <MDBCol md="6">
-                        !! Image !!
-                    </MDBCol>
-                    <MDBCol md="6">
-                        !! Image !!
-                    </MDBCol>
-                    <MDBCol md="6" className="pr-5 pl-5">
-                        <h2>Bla bla bla</h2>
-                        <p className="lead">Lead to catch attention</p>
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
-                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. Aliquam 
-                        erat volutpat. Ut a portarisus, eu porttitor nisi. Pellentesque
-                        turpis turpis, mattis lobortis justo sagittis, tempus ornare dui. Sed ornare nulla ac nulla 
-                        dapibus, eget vestibulum velit
-                        faucibus. Etiam auctor, augue ut vehicula porta, dui velit elementum nisi, a viverra enim 
-                        lectus eget nisl. Aenean laoreet
-                        in nibh eu commodo. Etiam augue sapien, tristique eget purus porttitor, egestas interdum 
-                        dolor. Sed odio ante, commodo eget dignissim nec, fermentum sed risus. Quisque imperdiet 
-                        nulla metus, vitae malesuada dolor pellentesque id. Vivamus condimentum consequat mattis. 
-                        Proin volutpat justo at tellus laoreet luctus vel eget risus.
-                        </p>
-                    </MDBCol>
-                    <MDBCol md="6" className="pr-5 pl-5">
-                        <h2>Bla bla bla</h2>
-                        <p className="lead">Lead to catch attention</p>
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
-                        venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. Aliquam 
-                        erat volutpat. Ut a portarisus, eu porttitor nisi. Pellentesque
-                        turpis turpis, mattis lobortis justo sagittis, tempus ornare dui. Sed ornare nulla ac nulla 
-                        dapibus, eget vestibulum velit
-                        faucibus. Etiam auctor, augue ut vehicula porta, dui velit elementum nisi, a viverra enim 
-                        lectus eget nisl. Aenean laoreet
-                        in nibh eu commodo. Etiam augue sapien, tristique eget purus porttitor, egestas interdum 
-                        dolor. Sed odio ante, commodo eget dignissim nec, fermentum sed risus. Quisque imperdiet 
-                        nulla metus, vitae malesuada dolor pellentesque id. Vivamus condimentum consequat mattis. 
-                        Proin volutpat justo at tellus laoreet luctus vel eget risus.
-                        </p>
-                    </MDBCol>
-                    <MDBCol md="6">
-                        !! Image !!
-                    </MDBCol>
-                    
-                </MDBRow>
+                <MDBContainer>
+                    <MDBRow className="m-0">
+                        <MDBCol md="6">
+                            !! Image !!
+                        </MDBCol>
+                        <MDBCol md="6" className="pr-5 pl-5">
+                            <h2>Bla bla bla</h2>
+                            <p className="lead">Lead to catch attention</p>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                            venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. Aliquam 
+                            erat volutpat. Ut a portarisus, eu porttitor nisi. Pellentesque
+                            turpis turpis, mattis lobortis justo sagittis, tempus ornare dui. Sed ornare nulla ac nulla 
+                            dapibus, eget vestibulum velit faucibus.
+                            </p>
+                        </MDBCol>
+                        <MDBCol md="6" className="pr-5 pl-5">
+                            <h2>Bla bla bla</h2>
+                            <p className="lead">Lead to catch attention</p>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                            venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. Aliquam 
+                            erat volutpat. Ut a portarisus, eu porttitor nisi. Pellentesque
+                            turpis turpis, mattis lobortis justo sagittis, tempus ornare dui. Sed ornare nulla ac nulla 
+                            dapibus, eget vestibulum velit faucibus.
+                            </p>
+                        </MDBCol>
+                        <MDBCol md="6">
+                            !! Image !!
+                        </MDBCol>
+                        <MDBCol md="6">
+                            !! Image !!
+                        </MDBCol>
+                        <MDBCol md="6" className="pr-5 pl-5">
+                            <h2>Bla bla bla</h2>
+                            <p className="lead">Lead to catch attention</p>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                            venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. Aliquam 
+                            erat volutpat. Ut a portarisus, eu porttitor nisi. Pellentesque
+                            turpis turpis, mattis lobortis justo sagittis, tempus ornare dui. Sed ornare nulla ac nulla 
+                            dapibus, eget vestibulum velit faucibus.
+                            </p>
+                        </MDBCol>
+                        <MDBCol md="6" className="pr-5 pl-5">
+                            <h2>Bla bla bla</h2>
+                            <p className="lead">Lead to catch attention</p>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus nisl ipsum, at 
+                            venenatis ante interdum sit amet. Sed sit amet tempor augue. Fusce at convallis massa. Aliquam 
+                            erat volutpat. Ut a portarisus, eu porttitor nisi. Pellentesque
+                            turpis turpis, mattis lobortis justo sagittis, tempus ornare dui. Sed ornare nulla ac nulla 
+                            dapibus, eget vestibulum velit faucibus.
+                            </p>
+                        </MDBCol>
+                        <MDBCol md="6">
+                            !! Image !!
+                        </MDBCol>
+                        
+                    </MDBRow>
+                </MDBContainer>
             </section>
         );
     }
