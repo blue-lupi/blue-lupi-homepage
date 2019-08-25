d84e1d7fbcafb9caa0f513e61fbc3bdc90a3da65
Christian Aichner <contact@aichner-christian.com>
2019-08-25 21:34:50 +0200

    Improve shop
    The shop has been improved. The shop cards are now
    being generated out of data.
    A problem has been resolved, that caused <p> tags
    to be inside of <p> tags. (MDBCardText is already a
    <p> element)

--- src/components/organisms/Sections/Shop/index.jsx
+++ src/components/organisms/Sections/Shop/index.jsx
@@ -29,73 +29,74 @@ import product3 from '../../../../assets/content/sections/shop/product3.png';
 //> CSS
 import './shop.scss';
 
+// Data
+const products = [
+    { 
+        name: "Blue Lupi 250g",
+        image: product1,
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        notice: "250g, geröstet und gemahlen",
+        btn: {
+            urlPath: "/test",
+            text: "Jetzt genießen"
+        }
+    },
+    { 
+        name: "Blue Lupi 500g",
+        image: product2,
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        notice: "500g, geröstet und gemahlen",
+        btn: {
+            urlPath: "/test",
+            text: "Jetzt genießen"
+        }
+    },
+    { 
+        name: "Blue Lupi Kapseln",
+        image: product3,
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        notice: "100g, bald verfügbar",
+        btn: {
+            urlPath: "/test",
+            text: "Jetzt genießen"
+        }
+    }
+];
+
 class Shop extends React.Component{
     render(){
         return(
             <section id="shop">
                 <MDBContainer>
                     <MDBRow className="text-center">
-                        <MDBCol>
-                            <MDBCard>
-                                <MDBCardImage
-                                className="img-fluid m-auto pl-5 pr-5 pt-3"
-                                src={product1}
-                                waves
-                                />
-                                <MDBCardBody>
-                                    <MDBCardTitle>Blue Lupi 250g</MDBCardTitle>
-                                    <MDBCardText>
-                                        <p>Some quick example text to build on the card title and make
-                                        up the bulk of the card&apos;s content.</p>
-                                        <small>250g, geröstet und gemahlen</small>
-                                    </MDBCardText>
-                                </MDBCardBody>
-                                <MDBCardFooter>
-                                    <MDBBtn color="lupi-blue" href="#">Jetzt genießen</MDBBtn>
-                                </MDBCardFooter>
-                                
-                            </MDBCard>
-                        </MDBCol>
-                        <MDBCol>
-                            <MDBCard>
-                                <MDBCardImage
-                                className="img-fluid m-auto pl-5 pr-5 pt-3"
-                                src={product2}
-                                waves
-                                />
-                                <MDBCardBody>
-                                <MDBCardTitle>Blue Lupi 500g</MDBCardTitle>
-                                <MDBCardText>
-                                    <p>Some quick example text to build on the card title and make
-                                    up the bulk of the card&apos;s content.</p>
-                                    <small>500g, geröstet und gemahlen</small>
-                                </MDBCardText>
-                                </MDBCardBody>
-                                <MDBCardFooter>
-                                    <MDBBtn color="lupi-blue" href="#">Jetzt genießen</MDBBtn>
-                                </MDBCardFooter>
-                            </MDBCard>
-                        </MDBCol>
-                        <MDBCol>
-                            <MDBCard>
-                                <MDBCardImage
-                                className="img-fluid m-auto pl-5 pr-5 pt-3"
-                                src={product3}
-                                waves
-                                />
-                                <MDBCardBody>
-                                <MDBCardTitle>Blue Lupi Kapseln</MDBCardTitle>
-                                <MDBCardText>
-                                    <p>Some quick example text to build on the card title and make
-                                    up the bulk of the card&apos;s content.</p>
-                                    <small>100, bald verfügbar</small>
-                                </MDBCardText>
-                                </MDBCardBody>
-                                <MDBCardFooter>
-                                    <MDBBtn color="lupi-blue" href="#">Jetzt genießen</MDBBtn>
-                                </MDBCardFooter>
-                            </MDBCard>
-                        </MDBCol>
+                    {products.map(({name, image, text, notice, btn}, i) => {
+                        return(
+                            <MDBCol key={i}>
+                                <MDBCard>
+                                    <MDBCardImage
+                                    className="img-fluid m-auto pl-5 pr-5 pt-3"
+                                    src={image}
+                                    waves
+                                    />
+                                    <MDBCardBody>
+                                    <MDBCardTitle>{name}</MDBCardTitle>
+                                    <MDBCardText>{text}</MDBCardText>
+                                    <MDBCardText><small>{notice}</small></MDBCardText>
+                                    </MDBCardBody>
+                                    <MDBCardFooter>
+                                        {btn.urlPath && 
+                                            <MDBBtn
+                                            color="lupi-blue"
+                                            href={btn.urlPath}
+                                            >
+                                            {btn.text}
+                                            </MDBBtn>
+                                        }
+                                    </MDBCardFooter>
+                                </MDBCard>
+                            </MDBCol>
+                        );
+                    })}
                     </MDBRow>
                 </MDBContainer>
             </section>
