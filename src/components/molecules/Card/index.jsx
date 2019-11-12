c273adadb4a4a54fc5e2c80e4f33c103e60f349d
Aichnerc <contact@aichner-christian.com>
2019-11-12 14:06:44 +0100

    Add Cart
    The cart molecule has been added.

---
+++ src/components/molecules/Card/index.jsx
@@ -0,0 +1,77 @@
+//> React
+// Contains all the functionality necessary to define React components
+import React from 'react';
+
+//> Components
+import {
+  LineItem
+} from '../../atoms';
+
+class Cart extends React.Component {
+  constructor(props) {
+  super(props);
+
+    this.openCheckout = this.openCheckout.bind(this);
+  }
+
+  openCheckout() {
+    window.open(this.props.checkout.webUrl);
+  }
+
+  render() {
+    let line_items = this.props.checkout.lineItems.edges.map((line_item) => {
+      return (
+        <LineItem
+          removeLineItemInCart={this.props.removeLineItemInCart}
+          updateLineItemInCart={this.props.updateLineItemInCart}
+          key={line_item.node.id.toString()}
+          line_item={line_item.node}
+        />
+      );
+    });
+
+    return (
+      <div className={`Cart ${this.props.isCartOpen ? 'Cart--open' : ''}`}>
+        <header className="Cart__header">
+          <h2>Cart</h2>
+          <button
+            onClick={this.props.handleCartClose}
+            className="Cart__close">
+            ×
+          </button>
+        </header>
+        <ul className="Cart__line-items">
+          {line_items}
+        </ul>
+        <footer className="Cart__footer">
+          <div className="Cart-info clearfix">
+            <div className="Cart-info__total Cart-info__small">Subtotal</div>
+            <div className="Cart-info__pricing">
+              <span className="pricing">$ {this.props.checkout.subtotalPrice}</span>
+            </div>
+          </div>
+          <div className="Cart-info clearfix">
+            <div className="Cart-info__total Cart-info__small">Taxes</div>
+            <div className="Cart-info__pricing">
+              <span className="pricing">$ {this.props.checkout.totalTax}</span>
+            </div>
+          </div>
+          <div className="Cart-info clearfix">
+            <div className="Cart-info__total Cart-info__small">Total</div>
+            <div className="Cart-info__pricing">
+              <span className="pricing">$ {this.props.checkout.totalPrice}</span>
+            </div>
+          </div>
+          <button className="Cart__checkout button" onClick={this.openCheckout}>Checkout</button>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Cart;
+
+/** 
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,10 +2,12 @@
 // Import all components to export them for easy access from parent components
 import Footer from './Footer';
 import Navbar from './Navbar';
+import Cart from './Card';
 
 export {
     Footer,
     Navbar,
+    Cart,
 };
 
 /** 
