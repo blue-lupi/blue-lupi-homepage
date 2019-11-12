a94630d5761a57e3b365d97d1c8de1d923f3c550
Aichnerc <contact@aichner-christian.com>
2019-11-12 14:07:09 +0100

    Add connection
    The HomePage has been adjusted to be able to
    receive data.

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -2,6 +2,28 @@
 // Contains all the functionality necessary to define React components
 import React from 'react';
 
+//> Additional
+// Prop types
+import PropTypes from 'prop-types';
+
+//> Apollo and GraphQL
+import { graphql } from 'react-apollo';
+import * as compose from 'lodash.flowright';
+import gql from 'graphql-tag';
+
+//> Queries
+import {
+  createCheckout,
+  checkoutLineItemsAdd,
+  checkoutLineItemsUpdate,
+  checkoutLineItemsRemove,
+  checkoutCustomerAssociate,
+  addVariantToCart,
+  updateLineItemInCart,
+  removeLineItemInCart,
+  associateCustomerCheckout
+} from '../../../utilities/checkout';
+
 //> Components
 // Hero
 import {
@@ -18,9 +40,105 @@ import {
   FAQ,
   Features,
 } from '../../organisms/Sections';
+// Shop
+import {
+  Cart
+} from '../../molecules';
 
 class HomePage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isCartOpen: false,
+      isCustomerAuthOpen: false,
+      isNewCustomer: false,
+      products: [],
+      checkout: { lineItems: { edges: [] } }
+    };
+
+    this.handleCartClose = this.handleCartClose.bind(this);
+    this.handleCartOpen = this.handleCartOpen.bind(this);
+    this.openCustomerAuth = this.openCustomerAuth.bind(this);
+    this.closeCustomerAuth = this.closeCustomerAuth.bind(this);
+    this.addVariantToCart = addVariantToCart.bind(this);
+    this.updateLineItemInCart = updateLineItemInCart.bind(this);
+    this.removeLineItemInCart = removeLineItemInCart.bind(this);
+    this.showAccountVerificationMessage = this.showAccountVerificationMessage.bind(this);
+    this.associateCustomerCheckout = associateCustomerCheckout.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.createCheckout({
+      variables: {
+        input: {}
+      }}).then((res) => {
+      this.setState({
+        checkout: res.data.checkoutCreate.checkout
+      });
+    });
+  }
+
+  static propTypes = {
+    data: PropTypes.shape({
+      loading: PropTypes.bool,
+      error: PropTypes.object,
+      shop: PropTypes.object,
+    }).isRequired,
+    createCheckout: PropTypes.func.isRequired,
+    checkoutLineItemsAdd: PropTypes.func.isRequired,
+    checkoutLineItemsUpdate: PropTypes.func.isRequired
+  }
+
+  handleCartOpen() {
+    this.setState({
+      isCartOpen: true,
+    });
+  }
+
+  handleCartClose() {
+    this.setState({
+      isCartOpen: false,
+    });
+  }
+
+  openCustomerAuth(event) {
+    if (event.target.getAttribute('data-customer-type') === "new-customer") {
+      this.setState({
+        isNewCustomer: true,
+        isCustomerAuthOpen: true
+      });
+    } else {
+      this.setState({
+        isNewCustomer: false,
+        isCustomerAuthOpen: true
+      });
+    }
+  }
+
+  showAccountVerificationMessage(){
+    this.setState({ accountVerificationMessage: true });
+    setTimeout(() => {
+      this.setState({
+        accountVerificationMessage: false
+      })
+    }, 5000);
+  }
+
+  closeCustomerAuth() {
+    this.setState({
+      isCustomerAuthOpen: false,
+    });
+  }
+
   render() {
+    if (this.props.data.loading) {
+      return <p>Loading ...</p>;
+    }
+    if (this.props.data.error) {
+      return <p>{this.props.data.error.message}</p>;
+    }
+
     return (
       <>
         <Hero />
@@ -33,12 +151,86 @@ class HomePage extends React.Component {
         <Shop />
         <Sub />
         <FAQ />
+        <Cart
+          removeLineItemInCart={this.removeLineItemInCart}
+          updateLineItemInCart={this.updateLineItemInCart}
+          checkout={this.state.checkout}
+          isCartOpen={this.state.isCartOpen}
+          handleCartClose={this.handleCartClose}
+          customerAccessToken={this.state.customerAccessToken}
+        />
       </>
     );
   }
 }
 
-export default HomePage;
+const query = gql`
+  query query {
+    shop {
+      name
+      description
+      products(first:20) {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+        }
+        edges {
+          node {
+            id
+            title
+            options {
+              id
+              name
+              values
+            }
+            variants(first: 250) {
+              pageInfo {
+                hasNextPage
+                hasPreviousPage
+              }
+              edges {
+                node {
+                  id
+                  title
+                  selectedOptions {
+                    name
+                    value
+                  }
+                  image {
+                    src
+                  }
+                  price
+                }
+              }
+            }
+            images(first: 250) {
+              pageInfo {
+                hasNextPage
+                hasPreviousPage
+              }
+              edges {
+                node {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const HomePageWithDataAndMutation = compose(
+  graphql(query),
+  graphql(createCheckout, {name: "createCheckout"}),
+  graphql(checkoutLineItemsAdd, {name: "checkoutLineItemsAdd"}),
+  graphql(checkoutLineItemsUpdate, {name: "checkoutLineItemsUpdate"}),
+  graphql(checkoutLineItemsRemove, {name: "checkoutLineItemsRemove"}),
+  graphql(checkoutCustomerAssociate, {name: "checkoutCustomerAssociate"})
+)(HomePage);
+
+export default HomePageWithDataAndMutation;
 
 /** 
  * SPDX-License-Identifier: (EUPL-1.2)
