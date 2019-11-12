bd999f07cc2dc36e72fb9a61febfdd104f78e92b
Aichnerc <contact@aichner-christian.com>
2019-11-13 00:35:35 +0100

    Improve LineItem
    The design has been improved.

--- src/components/atoms/LineItem/index.jsx
+++ src/components/atoms/LineItem/index.jsx
@@ -2,6 +2,16 @@
 // Contains all the functionality necessary to define React components
 import React from 'react';
 
+//> MDB
+// "Material Design for Bootstrap" is a great UI design framework
+import {
+  MDBBtn,
+  MDBIcon,
+} from 'mdbreact';
+
+//> CSS
+import './lineitem.scss';
+
 class LineItem extends React.Component {
   constructor(props) {
     super(props);
@@ -20,34 +30,61 @@ class LineItem extends React.Component {
 
   render() {
     return (
-      <li className="Line-item">
-        <div className="Line-item__img">
-          {this.props.line_item.variant.image && 
-          <img 
-          src={this.props.line_item.variant.image.src}
-          alt={`${this.props.line_item.title} product shot`}
-          />
-          }
-        </div>
-        <div className="Line-item__content">
-          <div className="Line-item__content-row">
-            <div className="Line-item__variant-title">
-              {this.props.line_item.variant.title}
-            </div>
-            <span className="Line-item__title">
-              {this.props.line_item.title}
-            </span>
+      <li className="line-item mt-2 pb-2">
+        <div className="row">
+          <div className="col-auto">
+            {this.props.line_item.variant.image && 
+            <img 
+            src={this.props.line_item.variant.image.src}
+            className="img-fluid"
+            alt={`${this.props.line_item.title} product shot`}
+            />
+            }
           </div>
-          <div className="Line-item__content-row">
-            <div className="Line-item__quantity-container">
-              <button className="Line-item__quantity-update" onClick={() => this.decrementQuantity(this.props.line_item.id)}>-</button>
-              <span className="Line-item__quantity">{this.props.line_item.quantity}</span>
-              <button className="Line-item__quantity-update" onClick={() => this.incrementQuantity(this.props.line_item.id)}>+</button>
+          <div className="col">
+            <div className="row flex-center">
+              <div className="col-auto text-left">
+                <p className="font-weight-bold mb-0">
+                {this.props.line_item.title}
+                </p>
+                <small>
+                {this.props.line_item.variant.title}
+                </small>
+              </div>
+              <div className="col text-right">
+                <MDBBtn 
+                color="danger"
+                size="sm"
+                onClick={()=> this.props.removeLineItemInCart(this.props.line_item.id)}
+                >
+                <MDBIcon icon="times" />
+                </MDBBtn>
+              </div>
             </div>
-            <span className="Line-item__price">
-              $ { (this.props.line_item.quantity * this.props.line_item.variant.price).toFixed(2) }
-            </span>
-            <button className="Line-item__remove" onClick={()=> this.props.removeLineItemInCart(this.props.line_item.id)}>×</button>
+            <div className="row flex-center">
+              <div className="col-auto text-left">
+                <div>
+                  <MDBBtn
+                  color="primary"
+                  size="sm"
+                  onClick={() => this.decrementQuantity(this.props.line_item.id)}
+                  >
+                  -
+                  </MDBBtn>
+                  <span className="ml-1 mr-1">{this.props.line_item.quantity}</span>
+                  <MDBBtn
+                  color="primary"
+                  size="sm"
+                  onClick={() => this.incrementQuantity(this.props.line_item.id)}
+                  >
+                  +
+                  </MDBBtn>
+                </div>
+              </div>
+              <div className="col text-right">
+                € { (this.props.line_item.quantity * this.props.line_item.variant.price).toFixed(2) }
+              </div>
+            </div>
           </div>
         </div>
       </li>
