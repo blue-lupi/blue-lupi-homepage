49f8e3192f91321e02b25a88cf323353f3ce5ab5
Aichnerc <contact@aichner-christian.com>
2019-11-13 15:35:08 +0100

    Improve Sub section
    The sub section has been improved.

--- src/components/organisms/Sections/Sub/index.jsx
+++ src/components/organisms/Sections/Sub/index.jsx
@@ -9,55 +9,31 @@ import { Parallax } from 'react-scroll-parallax';
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
 import {
+    MDBContainer,
     MDBRow,
     MDBCol,
 } from 'mdbreact';
 
 //> Images
-import moon from '../../../../assets/content/sections/sub/moon.png';
-import { ReactComponent as Wolf } from '../../../../assets/content/sections/sub/wolf.svg';
+import { ReactComponent as MoonAndWolf } from '../../../../assets/content/sections/sub/moonandwolf.svg';
 
 class Sub extends React.Component{
-
-    componentDidMount = () => {
-        let frames = document.querySelectorAll('.frame');
-        let currentFrame = 0;
-        let previousFrame = 7;
-
-        setInterval(function () {
-            frames[currentFrame].style.visibility = 'visible';
-            frames[previousFrame].style.visibility = 'hidden';
-
-            if (currentFrame < frames.length - 1) {
-                currentFrame += 1;
-                previousFrame = currentFrame - 1;
-            } else {
-                currentFrame = 0;
-                previousFrame = 7;
-            }
-        }, 75);
-    }
-
     render(){
         return(
             <section id="subscription">
-                <MDBRow className="m-0">
-                    <MDBCol md="3"></MDBCol>
-                    <MDBCol md="5">
-                        <h2 className="h2-responsive font-weight-bold">
-                        Wer mit dem Wolf geht lernt zu heulen.
-                        </h2>
-                        <p className="lead">Für den täglichen Genuss.</p>
-                        <div className="box">
-                            <Wolf />
-                        </div>
-                    </MDBCol>
-                    <MDBCol md="4">
-                        <Parallax className="parallax" y={[-40, 40]} tagOuter="figure">
-                            <img src={moon} alt="Mond"/>
-                        </Parallax>
-                    </MDBCol>
-                </MDBRow>
+                <MDBContainer>
+                    <MDBRow className="m-0 flex-center">
+                        <MDBCol md="8">
+                            <h2 className="h2-responsive font-weight-bold">
+                            Wer mit dem Wolf geht lernt zu heulen.
+                            </h2>
+                            <p className="lead">Für den täglichen Genuss.</p>
+                        </MDBCol>
+                        <MDBCol md="4">
+                            <MoonAndWolf id="wolfandmoon" />
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
             </section>
         );
     }
