5afcaf69b82dda58ce1295f867a55c0b4295a4f8
Christian Aichner <contact@aichner-christian.com>
2020-04-22 15:58:53 +0200

    Improve MessagePage
    Buttons have been added that can take you back to the 
    main page.

--- src/components/pages/MessagePage/index.jsx
+++ src/components/pages/MessagePage/index.jsx
@@ -2,64 +2,89 @@
 // Contains all the functionality necessary to define React components
 import React from "react";
 // Router
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
-import { MDBContainer } from "mdbreact";
+import { MDBContainer, MDBBtn } from "mdbreact";
 
 class MessagePage extends React.Component {
-	render() {
-		const { globalState } = this.props;
-		const data = globalState ? globalState.page : null;
+  render() {
+    const { globalState } = this.props;
+    const data = globalState ? globalState.page : null;
 
-		if (!data || !this.props.location) {
-			return (
-				<div className="py-5 my-5 flex-center">
-					<div className="spinner-border text-primary" role="status">
-						<span className="sr-only">Loading...</span>
-					</div>
-				</div>
-			);
-		} else {
-			return (
-				<MDBContainer id="message" className="py-5 my-5 text-center">
-					{this.props.location.pathname === "/about" && (
-						<div className="text-left">
-							<h2>Impressum</h2>
-							<p dangerouslySetInnerHTML={{ __html: data.about }}></p>
-						</div>
-					)}
-					{this.props.location.pathname === "/privacy" && (
-						<div className="text-left">
-							<h2>Datenschutzerklärung</h2>
-							<p dangerouslySetInnerHTML={{ __html: data.privacy }}></p>
-						</div>
-					)}
-					{this.props.location.pathname === "/agb" && (
-						<div className="text-left">
-							<h2>Allgemeine Geschäfts- und Nutzungsbedingungen</h2>
-							<p dangerouslySetInnerHTML={{ __html: data.gtc }}></p>
-						</div>
-					)}
-					{this.props.location.pathname === "/shipping" && (
-						<div className="text-left">
-							<h2>Versand- und Zahlungsinformationen</h2>
-							<p dangerouslySetInnerHTML={{ __html: data.shipping }}></p>
-						</div>
-					)}
-					{this.props.location.pathname === "/cancellation" && (
-						<div className="text-left">
-							<h2>Widerruf</h2>
-							<p
-								dangerouslySetInnerHTML={{ __html: data.cancellationPolicy }}
-							></p>
-						</div>
-					)}
-				</MDBContainer>
-			);
-		}
-	}
+    if (!data || !this.props.location) {
+      return (
+        <div className="py-5 my-5 flex-center">
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <MDBContainer id="message" className="py-5 my-5 text-center">
+          {this.props.location.pathname === "/about" && (
+            <div className="text-left">
+              <Link to="/">
+                <MDBBtn color="elegant" className="mb-4">
+                  Zurück
+                </MDBBtn>
+              </Link>
+              <h2>Impressum</h2>
+              <p dangerouslySetInnerHTML={{ __html: data.about }}></p>
+            </div>
+          )}
+          {this.props.location.pathname === "/privacy" && (
+            <div className="text-left">
+              <Link to="/">
+                <MDBBtn color="elegant" className="mb-4">
+                  Zurück
+                </MDBBtn>
+              </Link>
+              <h2>Datenschutzerklärung</h2>
+              <p dangerouslySetInnerHTML={{ __html: data.privacy }}></p>
+            </div>
+          )}
+          {this.props.location.pathname === "/agb" && (
+            <div className="text-left">
+              <Link to="/">
+                <MDBBtn color="elegant" className="mb-4">
+                  Zurück
+                </MDBBtn>
+              </Link>
+              <h2>Allgemeine Geschäfts- und Nutzungsbedingungen</h2>
+              <p dangerouslySetInnerHTML={{ __html: data.gtc }}></p>
+            </div>
+          )}
+          {this.props.location.pathname === "/shipping" && (
+            <div className="text-left">
+              <Link to="/">
+                <MDBBtn color="elegant" className="mb-4">
+                  Zurück
+                </MDBBtn>
+              </Link>
+              <h2>Versand- und Zahlungsinformationen</h2>
+              <p dangerouslySetInnerHTML={{ __html: data.shipping }}></p>
+            </div>
+          )}
+          {this.props.location.pathname === "/cancellation" && (
+            <div className="text-left">
+              <Link to="/">
+                <MDBBtn color="elegant" className="mb-4">
+                  Zurück
+                </MDBBtn>
+              </Link>
+              <h2>Widerruf</h2>
+              <p
+                dangerouslySetInnerHTML={{ __html: data.cancellationPolicy }}
+              ></p>
+            </div>
+          )}
+        </MDBContainer>
+      );
+    }
+  }
 }
 
 export default withRouter(MessagePage);
