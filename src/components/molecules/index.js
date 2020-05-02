454ea470aade5e78cf28eff1d50c5b27be948151
Aichnerc <contact@aichner-christian.com>
2020-05-02 22:33:47 +0200

    Improve code quality
    Some indentation has been fixed.

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,16 +1,12 @@
 //> Components
 // Import all components to export them for easy access from parent components
-import Footer from './Footer';
-import Cart from './Cart';
-import Product from './Product';
+import Footer from "./Footer";
+import Cart from "./Cart";
+import Product from "./Product";
 
-export {
-    Footer,
-    Cart,
-    Product,
-};
+export { Footer, Cart, Product };
 
-/** 
+/**
  * SPDX-License-Identifier: (EUPL-1.2)
  * Copyright © 2019-2020 Werbeagentur Christian Aichner
  */
